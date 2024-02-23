6af4c3ede462fb4429062274396336e2f956fb09
jeka boyko <84972844+jekaboss@users.noreply.github.com>
2024-02-23 11:09:16 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 // Строгий режим
 "use strict"
+// Language data
+const languageData = {
+    'en': {
+        'heading': 'Hello, World!',
+        'paragraph': 'This is a sample paragraph.'
+    },
+    'ru': {
+        'heading': 'Привет, мир!',
+        'paragraph': 'Это пример параграфа.'
+    }
+};
+
+// Function to change language
+function changeLanguage(selectedLanguage) {
+    const elements = Object.keys(languageData[selectedLanguage]);
+    elements.forEach(element => {
+        document.getElementById(element).innerText = languageData[selectedLanguage][element];
+    });
+}
+
+// Initialize content with default language
+changeLanguage('en');
